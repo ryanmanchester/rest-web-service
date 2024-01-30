65f8f9f531ca501a737184427b8bd0e0877013c7
Ryan Manchester <ryanm17@gmail.com>
2024-01-30 13:40:54 -0500

    initial get mapping for hello world controller

---
+++ restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.ryanmanchester.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+
+}
