6ad36cdf2f7b2ab54c31d28ddacf6c559e19b3f2
Ryan Manchester <ryanm17@gmail.com>
2024-01-30 14:02:42 -0500

    using path variables to capture data from path

--- restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.ryanmanchester.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,6 +16,11 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World!");
+	}
+	
+	@GetMapping("/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean("Hello " + name);
 	}
 
 }
