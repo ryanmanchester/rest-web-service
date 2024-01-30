f47e8c5e1d4a3c8c8359503207e9aed437f9d9d5
Ryan Manchester <ryanm17@gmail.com>
2024-01-30 14:24:32 -0500

    retrieves all users in user resource from static list in user dao

--- restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,21 @@
 package com.ryanmanchester.rest.webservices.restfulwebservices.user;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserResource {
+	private UserDaoService userService;
+	
+	public UserResource(UserDaoService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return userService.findAll();
+	}
 
 }
