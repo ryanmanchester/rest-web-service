f0bac659715bfd5b9ebd91d32c195a6add75ed12
Ryan Manchester <ryanm17@gmail.com>
2024-01-30 15:07:51 -0500

    configures createUser method to return a 201 status and uri of new user

--- restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.ryanmanchester.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -27,8 +30,13 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		userService.saveUser(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = userService.saveUser(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(savedUser.getId())
+						.toUri();
+		return ResponseEntity.created(location ).build();
 	}
 
 }
