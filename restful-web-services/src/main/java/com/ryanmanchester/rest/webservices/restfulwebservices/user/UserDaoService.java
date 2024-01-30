a311b4b8fa2638a33b8be23954586c8ee5830ac5
Ryan Manchester <ryanm17@gmail.com>
2024-01-30 14:18:52 -0500

    creates user bean, dao service, and resource to mock user data

---
+++ restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,23 @@
+package com.ryanmanchester.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	
+	private static List<User> users = new ArrayList<>();
+	static {
+		users.add(new User(1, "Ryan", LocalDate.now().minusYears(38)));
+		users.add(new User(2, "Cameron", LocalDate.now().minusYears(34)));
+		users.add(new User(3, "Madison", LocalDate.now().minusYears(36)));
+	}
+	
+	public List<User> findAll() {
+		return users;
+	}
+
+}
---
+++ restful-web-services/src/main/java/com/ryanmanchester/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,8 @@
+package com.ryanmanchester.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+}
